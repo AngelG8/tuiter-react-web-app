30388036028e2758ae5ed1e516c5fec954d35c89
angel <63884758+AngelG8@users.noreply.github.com>
2023-05-29 03:52:47 -0500

    Revert merge a3 into a2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import Labs from "./labs";
-import HelloWorld from "./labs/a3/hello-world";
-import Tuiter from "./tuiter";
-import { BrowserRouter, Navigate } from "react-router-dom";
-import { Routes, Route } from "react-router";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={<Navigate to="/labs" />} />
-
-                    <Route path="/labs/*" element={<Labs />} />
-                    <Route path="/hello" element={<HelloWorld />} />
-                    <Route path="/tuiter/*" element={<Tuiter />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
