b48a33fc70d4bc9b33c61f82674cde591fd87ccb
angel <63884758+AngelG8@users.noreply.github.com>
2023-06-13 21:23:59 -0500

    Fixed update user routing

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -21,7 +21,7 @@ export const profile = async () => {
 };
 
 export const updateUser = async (user) => {
-    const response = await api.put(`${USERS_URL}/${user._id}`, user);
+    const response = await api.put(`${USERS_URL}`, user);
     return response.data;
 };
 
